b752811cfdec564c49325ce3e7495f55588e3cbd
samee-k <sameeksha@fusemachines.com>
2026-02-18 18:09:55 +0545

    updated: baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://talktodbdev.flo.io/login',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
     videosFolder: 'cypress/videos',
