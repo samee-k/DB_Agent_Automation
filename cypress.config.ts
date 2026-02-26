c61a4334b56105be20a106fa63b303583c75fd66
samee-k <sameeksha@fusemachines.com>
2026-02-26 15:22:41 +0545

    update: dbAgent baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://talktodbdev.flo.io/login',
+    baseUrl: 'https://talktodbdev.flo.io',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
     videosFolder: 'cypress/videos',
